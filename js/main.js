3b71056a7bd7f47b36ba0959934ccfedc3d66c8c
udit01 <uditjainiitd@gmail.com>
2017-06-04 19:12:42 +0530

    done!

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-video = document.getElementById("bg_video")
+// created by udit01
 
-function detectmob() {
-   console.log(window.innerWidth);
-     console.log(window.innerHeight);
-   if($(window).width() <= 1200 || $(window).height() <= 1200) {
-     return true;
-   } else {
-     return false;
-   }
-}
 
-if (detectmob())
-{
-  console.log("video removed");
-  video.remove();
-}
+// video = document.getElementById("bg_video")
+//
+// function detectmob() {
+//    console.log(window.innerWidth);
+//      console.log(window.innerHeight);
+//    if($(window).width() <= 1200 || $(window).height() <= 1200) {
+//      return true;
+//    } else {
+//      return false;
+//    }
+// }
+//
+// if (detectmob())
+// {
+//   console.log("video removed");
+//   video.remove();
+// }
